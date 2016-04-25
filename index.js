dd36c902fd7a95fb5bf56a20d47356d8d5e8ee05
Ionut Cirja <ionut.cirja@gmail.com>
2016-04-25 11:57:04 +0100

    backbone decorator

---
+++ index.js
@@ -0,0 +1,87 @@
+var Backbone = require('backbone');
+var _ = require('underscore');
+
+function addDecorators(entity, decorators) {
+
+    decorators.forEach(function(decorator) {
+        if (typeof decorator === 'function') {
+            decorate(entity, decorator(entity));
+        } else {
+            throw new Error('Decorator must be a function');
+        }
+    });
+}
+
+function decorate(entity, decorator) {
+
+    if (typeof decorator === 'object') {
+
+        _.keys(decorator).forEach(function(key) {
+            if (typeof decorator[key] !== 'object' && typeof decorator[key] !== 'function') {
+                throw new Error('You cannot decorate with type ' + typeof decorator[key]);
+            }
+
+            if (!entity[key]) {
+                entity[key] = decorator[key];
+            } else {
+                var property = entity[key];
+                if (typeof property !== typeof decorator[key]) {
+                    throw new Error('You cannot decorate a type ' + typeof property + ' with a type ' + typeof decorator[key]);
+                }
+
+                if (typeof property === 'function') {
+                    entity[key] = _.compose(decorator[key], property);
+                }
+
+                if (typeof property === 'object') {
+                    entity[key] = _.extend(property, decorator[key]);
+                }
+            }
+
+        });
+    }
+
+    return decorator;
+}
+
+function extend(entity, options) {
+
+    var decorators = entity.decorators || (options ? options.decorators : {}) || {};
+
+    if (decorators.length) {
+        addDecorators(entity, decorators);
+    }
+}
+
+Backbone.View = (function(View) {
+
+    return View.extend({
+        constructor: function(options) {
+            extend(this, options);
+            View.apply(this, arguments);
+        }
+    });
+
+})(Backbone.View);
+
+Backbone.Model = (function(Model) {
+
+    return Model.extend({
+        constructor: function(attributes, options) {
+            extend(this, options);
+            Model.apply(this, arguments);
+        }
+    });
+
+})(Backbone.Model);
+
+Backbone.Collection = (function(Collection) {
+
+    return Collection.extend({
+        constructor: function(models, options) {
+            extend(this, options);
+            Collection.apply(this, arguments);
+        }
+    });
+
+})(Backbone.Collection);
---
+++ tests/index.spec.js
@@ -0,0 +1,110 @@
+var Backbone = require('backbone');
+require('../index');
+var expect = require('chai').expect;
+
+describe('Backbone Decorator', function() {
+
+    it('should attach decorators to a backbone entity if an decorators object is passed to the constructor', function() {
+        function decorator() {
+            return {
+                decoratorMethod: function() {
+                    return 'some value';
+                }
+            }
+        }
+        var view = new Backbone.View({decorators: [decorator]});
+        var model = new Backbone.Model({}, {decorators: [decorator]});
+        var collection = new Backbone.Collection([], {decorators: [decorator]});
+
+        expect(view.decoratorMethod()).to.equal('some value');
+        expect(model.decoratorMethod()).to.equal('some value');
+        expect(collection.decoratorMethod()).to.equal('some value');
+    });
+
+    it('should throw an error if something else then a function is passed as an decorator', function() {
+        expect(function() {
+            new Backbone.View({
+                decorators: [
+                    { module : 'string' }
+                ]
+            });
+        }).to.throw('Decorator must be a function');
+    });
+
+    it('should compose decorators object if two decorators are exposing the same name for one or more properties', function() {
+        function decorator() {
+            return {
+                someObj: {someProperty: 'same value'}
+            }
+        }
+        function anotherDecorator() {
+            return {
+                someObj: {otherProperty: 'other value'}
+            }
+        }
+        var view = new Backbone.View({
+            decorators: [decorator, anotherDecorator]
+        });
+
+        expect(view.someObj).to.eql({someProperty: 'same value', otherProperty: 'other value'});
+    });
+
+    it('should compose decorators methods if the decorators are exposing the same name for one or more methods', function() {
+        function decorator() {
+            return {
+                someMethod: function(num) {
+                    return num + 1;
+                }
+            }
+        }
+        function anotherDecorator() {
+            return {
+                someMethod: function(num) {
+                    return num + 2;
+                }
+            }
+        }
+        var View = Backbone.View.extend({
+            decorators: [decorator, anotherDecorator],
+            someMethod: function() {
+                return 1;
+            }
+        });
+
+        var view = new View();
+        expect(view.someMethod()).to.equal(4);
+    });
+
+    it('should throw an error if a decorator is exposing something else than an object or a function', function() {
+        function decorator() {
+            return {
+                invalidProperty: 'string'
+            }
+        }
+
+        expect(function() {
+            new Backbone.View({
+                decorators: [decorator]
+            });
+        }).to.throw('You cannot decorate with type string');
+    });
+
+    it('should throw an error if an existing method or object is decorated with another type than the current one', function() {
+        function decorator() {
+            return {
+                property: {someKey: 'some value'}
+            }
+        }
+        function anotherDecorator() {
+            return {
+                property: function(){}
+            }
+        }
+
+        expect(function() {
+            new Backbone.View({
+                decorators: [decorator, anotherDecorator]
+            });
+        }).to.throw('You cannot decorate a type object with a type function');
+    });
+});
